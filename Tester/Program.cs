e2f355d55c7d0f9cce34aa71d42f6fd30c805e2b
Burak Selim Senyurt (Dogus Teknoloji) <BurakSelim.Senyurt@d-teknoloji.com.tr>
2020-09-18 10:27:58 +0300

    Test kodu eklendi

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Denver.Facade.Parts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,14 @@ namespace Tester
     {
         static void Main(string[] args)
         {
+            #region Parça Ekleme Testi
+
+            PartManagerFacade partManagerFacade = new PartManagerFacade();
+            var retCode=partManagerFacade.AddToStock(9, 100, 90.95, 1000, "Vakum Tüpü", 500, "Rio Vakum Tüpleri Firması", "Sanayi tipi 500 miliwatt çekim gücüne sahip vakum tüpüdür");
+            Console.WriteLine("Parça eklenme durumu ->" + retCode.ToString());
+
+
+            #endregion
         }
     }
 }
