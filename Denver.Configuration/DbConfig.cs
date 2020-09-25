e398b11cd96e3852ff2e3b8065ad7c744e31d06a
Burak Selim Senyurt (Dogus Teknoloji) <BurakSelim.Senyurt@d-teknoloji.com.tr>
2020-09-25 10:05:44 +0300

    Bağlantı adresi değiştirildi

--- Denver.Configuration/DbConfig.cs
+++ Denver.Configuration/DbConfig.cs
@@ -7,7 +7,7 @@ namespace Denver.Configuration
 {
     public static class DbConfig
     {
-        public static string ConnectionString = "data source=localhost;database=DenverDb;integrated security=SSPI;";
+        public static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\project-denver\Database\DenverDb.mdf;Integrated Security = True; Connect Timeout = 30";
         public static string LogDbConnectionString = "data source=localhost:database=LogDB;user id=logger;password=AlQ390!#f;";
     }
 }
