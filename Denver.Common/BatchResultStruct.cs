91818af46d240771273bec2d2f5e00ae884df963
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-03-05 19:33:26 +0300

    Automic batch'ler için tanımlamalar.

---
+++ Denver.Common/BatchResultStruct.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Denver.Common.Services
+{
+    public class BatchResultStruct
+    {
+        public int ReturnCode { get; set; }
+        public DataSet DataReturned { get; set; }
+    }
+}
